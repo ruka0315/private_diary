477b444c40196c96566bfcfb3d8bcfe5732385a5
ruka0315 <ksw207032@stu.o-hara.ac.jp>
2021-09-07 12:34:24 +0900

    お問い合わせフォーム

--- diary/views.py
+++ diary/views.py
@@ -1,5 +1,12 @@
 from django.views import generic
 
+from.froms import InquiryForm
+
 # Create your views here.
 class IndexView(generic.TemplateView):
     template_name = "index.html"
+
+
+class InquiryView(generic.FromView):
+    template_name = "inquiry.html"
+    form_class = InquiryForm
